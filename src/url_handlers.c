ac000/dnsandmx_admin

commit 5835ab826df2334d8b9b281bf4ac4008ba99489d

url_handlers: Convert delete_dns_domain to is_users_domain
This required moving the mysql result freeing around and we also fix a
potential memory leak of the db_row variable when we might have jumped
out of the function before freeing it.

Signed-off-by: Andrew Clayton <andrew@dnsandmx.com>

src/url_handlers.c
@@ -841,12 +841,8 @@ static void delete_dns_domain(void)
 	GHashTable *db_row = NULL;
 
 	domain_id = atoi(qvar("domain_id"));
-
-	res = sql_query(conn, "SELECT domain_id FROM domains WHERE uid = %u "
-			"AND domain_id = %d", user_session.uid, domain_id);
-	if (mysql_num_rows(res) == 0)
+	if (!is_users_domain(domain_id, "domains"))
 		goto out;
-	mysql_free_result(res);
 
 	res = sql_query(conn, "SELECT pdns.domains.name AS domain FROM "
 			"pdns.domains WHERE pdns.domains.id = %d",
@@ -856,6 +852,8 @@ static void delete_dns_domain(void)
 	vl = add_html_var(vl, "domain", domain);
 	vl = add_html_var(vl, "domain_id", qvar("domain_id"));
 	vl = add_html_var(vl, "username", user_session.username);
+	free_vars(db_row);
+	mysql_free_result(res);
 
 	if (IS_POST() && valid_csrf_token()) {
 		int ret;
@@ -883,14 +881,12 @@ static void delete_dns_domain(void)
 			vl = add_html_var(vl, "auth_err", "yes");
 		}
 	}
-	free_vars(db_row);
 
 out:
 	add_csrf_token(vl);
 	fmtlist = TMPL_add_fmt(fmtlist, "de_xss", de_xss);
 	send_template("templates/delete_dns_domain.tmpl", vl, fmtlist);
 out2:
-	mysql_free_result(res);
 	TMPL_free_varlist(vl);
 	TMPL_free_fmtlist(fmtlist);
 }

commit d0119ec32bb742ff72b49c98893a75c1c6fa0776

url_handlers: delete_dns_domain: Fix DNS domain deletion
Commit 5835ab8 ("url_handlers: Convert delete_dns_domain to
is_users_domain") introduced a subtle bug when deleting DNS domains from
the secondary name server.

We were assigning a pointer to the domain name, however the variable
that the pointer was pointing to was being free'd before we'd finished
using it and thus got messed up.

strdupa(3) the 'domain' variable to solve this.

Signed-off-by: Andrew Clayton <andrew@dnsandmx.com>

src/url_handlers.c
@@ -833,7 +833,7 @@ out:
 static void delete_dns_domain(void)
 {
 	int domain_id;
-	const char *domain;
+	char *domain;
 	MYSQL_RES *res;
 	TMPL_varlist *vl = NULL;
 	TMPL_fmtlist *fmtlist = NULL;
@@ -847,7 +847,7 @@ static void delete_dns_domain(void)
 			"pdns.domains WHERE pdns.domains.id = %d",
 			domain_id);
 	db_row = get_dbrow(res);
-	domain = get_var(db_row, "domain");
+	domain = strdupa(get_var(db_row, "domain"));
 	vl = add_html_var(vl, "domain", domain);
 	vl = add_html_var(vl, "domain_id", qvar("domain_id"));
 	vl = add_html_var(vl, "username", user_session.username);