ac000/dnsandmx_admin

commit 8abb36059fbba125f045a2980b15b55bdb8e761a

url_handlers: Convert delete_mail_domain to is_users_domain
Use the is_users_domain() function to check the domain belongs to the
user.

This required moving the mysql result freeing around and we also fix a
potential memory leak of the db_row variable when we might have jumped
out of the function before freeing it.

Signed-off-by: Andrew Clayton <andrew@dnsandmx.com>

src/url_handlers.c
@@ -490,13 +490,8 @@ static void delete_mail_domain(void)
 	GHashTable *db_row = NULL;
 
 	domain_id = atoi(qvar("domain_id"));
-
-	res = sql_query(conn, "SELECT domain_id FROM mail_domains WHERE "
-			"uid = %u AND domain_id = %d", user_session.uid,
-			domain_id);
-	if (mysql_num_rows(res) == 0)
+	if (!is_users_domain(domain_id, "mail_domains"))
 		goto out;
-	mysql_free_result(res);
 
 	res = sql_query(conn, "SELECT domain, type FROM mail_domains WHERE "
 				"domain_id = %d", domain_id);
@@ -506,6 +501,8 @@ static void delete_mail_domain(void)
 	vl = add_html_var(vl, "domain", domain);
 	vl = add_html_var(vl, "domain_id", qvar("domain_id"));
 	vl = add_html_var(vl, "username", user_session.username);
+	free_vars(db_row);
+	mysql_free_result(res);
 
 	if (IS_POST() && valid_csrf_token()) {
 		int ret;
@@ -540,14 +537,12 @@ static void delete_mail_domain(void)
 			vl = add_html_var(vl, "auth_err", "yes");
 		}
 	}
-	free_vars(db_row);
 
 out:
 	add_csrf_token(vl);
 	fmtlist = TMPL_add_fmt(fmtlist, "de_xss", de_xss);
 	send_template("templates/delete_mail_domain.tmpl", vl, fmtlist);
 out2:
-	mysql_free_result(res);
 	TMPL_free_varlist(vl);
 	TMPL_free_fmtlist(fmtlist);
 }

commit 1a986c9da412242fc6fadddd43e028232e2a1425

url_handlers: delete_mail_domain: Fix mail domain deletion
Commit 8abb360 ("url_handlers: Convert delete_mail_domain to
is_users_domain") introduced a subtle bug when deleting mail domains.

We were assigning a pointer to the mail domain type, however the
variable that the pointer was pointing to was being free'd before we'd
finished using the pointer and thus got messed up.

strdupa(3) the 'type' variable to solve this.

Signed-off-by: Andrew Clayton <andrew@dnsandmx.com>

src/url_handlers.c
@@ -483,7 +483,7 @@ static void delete_mail_domain(void)
 {
 	int domain_id;
 	const char *domain;
-	const char *type;
+	char *type;
 	MYSQL_RES *res;
 	TMPL_varlist *vl = NULL;
 	TMPL_fmtlist *fmtlist = NULL;
@@ -496,7 +496,7 @@ static void delete_mail_domain(void)
 	res = sql_query(conn, "SELECT domain, type FROM mail_domains WHERE "
 				"domain_id = %d", domain_id);
 	db_row = get_dbrow(res);
-	type = get_var(db_row, "type");
+	type = strdupa(get_var(db_row, "type"));
 	domain = get_var(db_row, "domain");
 	vl = add_html_var(vl, "domain", domain);
 	vl = add_html_var(vl, "domain_id", qvar("domain_id"));